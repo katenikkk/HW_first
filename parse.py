a6daf4757de8f6234908bdcb506c2b07764bcf76
Kate <nikolutina12@gmail.com>
2021-08-01 01:52:30 +0300

    parse test

--- parse.py
+++ parse.py
@@ -9,3 +9,22 @@ def parse(query: str) -> dict:
     dictionary = dict(zip(key, value))
     print(dictionary)
     return{dict}
+
+if __name__ == '__main__':
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('http://example.com/') == {}
+    assert parse('http://example.com/?') == {}
+    assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
+
+    assert parse('http://example.com/?name=ferret&') == {'name': 'ferret'}
+    assert parse('http://example.com/?color=purple&&') == {'color': 'purple'}
+    assert parse('http://example.com/?color=purple&&name=ferret&&') == {'color': 'purple', 'name': 'ferret'}
+    assert parse('http://example.com/?name=ferret&name==Dima&color==purple') == {'name': 'ferret', 'name': 'Dima'}
+    assert parse('http://example.com/?&') == {}
+    assert parse('http://example.com/?&&name=ferret&name=Dima&&') == {'name': 'ferret', 'name': 'Dima'}
+    assert parse('http://example.com/?&==&') == {}
+    assert parse('http://example.com/?name===Bob&&') == {'name': 'Bob'}
+    assert parse('http://example.com/?home==&') == {}
+    assert parse('http://example.com/?&&&month==april&&&') == {'month': 'april'}
+
